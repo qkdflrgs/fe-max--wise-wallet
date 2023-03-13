c72921815015f78f44fa5d9fcf27f4e4a0c0fe67
hjsong <song6240@naver.com>
2023-03-14 04:51:04 +0900

    feat: 헤더 연월 정보 초기화,좌우버튼 이벤트 설정
    Date 객체로 연월 정보 가져옴.
    Node.textContent로 초기화.
    Date 메서드로 달 정보 변경하는 함수를 만들고 이벤트 핸들러에서 실행.

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import { initCarouselButton, initMonthInfo } from "./components/header.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initMonthInfo();
+  initCarouselButton();
+});
