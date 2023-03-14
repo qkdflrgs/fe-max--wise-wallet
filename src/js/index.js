a48971f36480d5ee70716bcbe7f65dc5c0622e45
hjsong <song6240@naver.com>
2023-03-14 21:32:04 +0900

    feat: 금액 부호 버튼 css 추가
    마우스 호버 커서 포인터로 변경.
    코드 가시성을 위해 css selector를 id로 간략화함.

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,10 @@ import { initDefaultDate } from "./components/dateInput.js";
 import { addButtonEventListener, initMonthView } from "./components/monthCarousel.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // 헤더 연월 carousel
   initMonthView();
   addButtonEventListener();
 
+  // 일자 입력란
   initDefaultDate();
 });
