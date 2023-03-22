465b173efdd62e28f278ff5d899c3f6d9b915203
hjsong <song6240@naver.com>
2023-03-22 19:45:54 +0900

    feat: 내역 객체를 로컬스토리지에 저장
    인풋바의 모든 입력값을 모아 내역 객체를 만들고 로컬 스토리지에 저장함.
    연월값을 key로 로컬스토리지에 저장하고 value로는 일자와 내역 객체 배열을 key, value로 하는 객체를 문자열화해서 저장함.
    저장 후 인풋바의 입력란들을 초기화함.

--- src/js/components/inputBar/validator.js
+++ src/js/components/inputBar/validator.js
@@ -1,55 +1,97 @@
 import { PAYMENT_TYPES } from "../../constants/paymentTypes.js";
 import { getCategories } from "../../constants/categories.js";
 import { getNumberWithoutComma } from "../../utils/utils.js";
+import { initDefaultDate } from "./dateInput.js";
+import { initDefaultPrice } from "./priceInput.js";
+import { deactivateSelectBox } from "./dropdown.js";
 
 const inputBar = document.querySelector(".input-bar");
+const dateInput = inputBar.querySelector("#input-date");
+const priceInput = inputBar.querySelector("#input-price");
+const signInput = inputBar.querySelector("#sign-switcher");
+const contentInput = inputBar.querySelector("#input-content");
+const paymentTypeInput = inputBar.querySelector("#input-payment-type");
+const categoryInput = inputBar.querySelector("#input-category");
 const checkButton = inputBar.querySelector(".input-bar__check-button");
 
 const isValidDate = () => {
-  const date = inputBar.querySelector("#input-date").value;
+  const date = dateInput.value;
   const regex = /^\d{4}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])$/;
   return regex.test(date);
 };
 
 const isValidPrice = () => {
-  const price = inputBar.querySelector("#input-price").value;
-  const sign = inputBar.querySelector("#sign-switcher").checked ? -1 : 1;
+  const price = priceInput.value;
+  const sign = signInput.checked ? -1 : 1;
   return price * sign !== 0;
 };
 
 const isValidContent = () => {
-  const content = inputBar.querySelector("#input-content").value;
+  const content = contentInput.value;
   return typeof content === "string" && content.trim().length !== 0;
 };
 
-const isValidPayment = () => {
-  const payment = inputBar.querySelector("#input-payment-type").value;
-  return PAYMENT_TYPES.includes(payment);
+const isValidPaymentType = () => {
+  const paymentType = paymentTypeInput.value;
+  return PAYMENT_TYPES.includes(paymentType);
 };
 
 const isValidCategory = () => {
-  const category = inputBar.querySelector("#input-category").value;
-  const isOutcome = inputBar.querySelector("#sign-switcher").checked;
+  const category = categoryInput.value;
+  const isOutcome = signInput.checked;
   return getCategories(isOutcome).includes(category);
 };
 
 const checkAllInputValues = () => {
-  if (isValidDate() && isValidPrice() && isValidContent() && isValidPayment() && isValidCategory()) {
-    checkButton.classList.toggle("active");
+  if (isValidDate() && isValidPrice() && isValidContent() && isValidPaymentType() && isValidCategory()) {
+    checkButton.classList.add("active");
+    return;
   }
+  checkButton.classList.remove("active");
 };
 
-const saveInOutcome = () => {
-  const inOutcome = {
-    date: inputBar.querySelector("#input-date").value,
-    price: getNumberWithoutComma(inputBar.querySelector("#input-price").value),
-    isOutcome: inputBar.querySelector("#sign-switcher").checked,
-    paymentType: inputBar.querySelector("#input-payment-type").value,
-    category: inputBar.querySelector("#input-category").value,
-  }
-}
+const getInOutcome = () => {
+  return {
+    date: dateInput.value,
+    price: getNumberWithoutComma(priceInput.value),
+    isOutcome: signInput.checked,
+    content: contentInput.value,
+    paymentType: paymentTypeInput.value,
+    category: categoryInput.value,
+  };
+};
 
-// inputBar.addEventListener("click", checkAllInputValues);
+const getMonthInOutcomes = (month) => {
+  const data = localStorage.getItem(month);
+  return JSON.parse(data) ?? {};
+};
+
+const resetInputBar = () => {
+  initDefaultDate();
+  initDefaultPrice();
+  contentInput.value = "";
+
+  const dropdowns = inputBar.querySelectorAll(".dropdown");
+  dropdowns.forEach((d) => {
+    deactivateSelectBox(d);
+  });
+};
+
+const saveInOutcome = (e) => {
+  e.preventDefault();
+
+  const inOutcome = getInOutcome();
+  const monthYear = inOutcome.date.slice(0, -2);
+  const monthData = getMonthInOutcomes(monthYear);
+  const dateList = monthData[inOutcome.date] ?? [];
+
+  dateList.push(inOutcome);
+  monthData[inOutcome.date] = dateList;
+  localStorage.setItem(monthYear, JSON.stringify(monthData));
+
+  resetInputBar();
+};
+
 inputBar.addEventListener("input", checkAllInputValues);
 inputBar.addEventListener("click", checkAllInputValues);
-checkButton.addEventListener('click', saveInOutcome);
+checkButton.addEventListener("click", saveInOutcome);
